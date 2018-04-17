823f3c5cd3f4f90df17f4e96829845baf939c952
Gabe Johnson <gabe@blankslatesystems.com>
2018-04-17 11:48:22 -0600

    Priority Queue initial commit

---
+++ pq.cpp
@@ -0,0 +1,32 @@
+//
+// pq.cpp
+//
+// Decide how you want to implement this: linked list? heap? array?
+// magic pixie dust? The choice is yours.
+//
+// NOTE: You will need to edit pq_struct.h as appropriate, depending
+// on your implementation strategy. You will turn in this file (your
+// implementation of everything) and pq_struct.h (your definition of
+// the pq structure and any helper functions you might create).
+
+// Your Name:
+
+
+#include <string>
+#include "pq.h"
+
+pq* init_priority_queue() {
+  
+}
+
+void insert(pq* &queue, string text, float priority) {
+
+}
+
+string remove(pq* &queue) {
+  return "TODO"; // TODO
+}
+
+string peek(pq* &queue) {
+  return "TODO"; // TODO
+}
